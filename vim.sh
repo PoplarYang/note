8fbcf5029cea9dd7e06ba0f134ec5ce063213287
PoplarTree <echohiyang@foxmail.com>
2020-01-19 23:48:53 +0800

    add vim

---
+++ vim.sh
@@ -0,0 +1,12 @@
+set fileencodings=utf-8,ucs-bom,gb18030,gbk,gb2312,cp936
+set fencs=utf8,gbk,gb2312,gb18030
+set termencoding=utf-8
+set encoding=utf-8
+set enc=utf8
+
+set autoindent     " 换行自动缩进
+set shiftwidth=4   " 自动缩进时的宽度
+set ts=4           " tabstop的宽度
+set expandtab      " tab换成空格
+set softtabstop=4  " 退格键
+"set paste          " 粘贴时不自动换行
